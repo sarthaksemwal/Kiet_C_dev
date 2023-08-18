45875226e1cd64db9d2eb6371e7bff09b6b77297
sarthaksemwal <109014470+sarthaksemwal@users.noreply.github.com>
2023-08-18 11:38:49 +0530

    Create Sum.c

---
+++ Sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    //declaring the variable a,b,sum where sum stores the value of a+b
+    int a, b, sum;
+    a = 10;
+    b = 20;
+    sum = a + b;
+    //printing the output
+    printf("sum is: %d\n", sum);
+    return 0;
+}
