cc125813a8bf773ee01b2bbed64b7e660e57ad6b
sarthaksemwal <109014470+sarthaksemwal@users.noreply.github.com>
2023-08-22 20:45:44 +0530

    Create Using_IO_fun.c

---
+++ Using_IO_fun.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+//program starts execution form here
+
+int main() {
+    int c;
+    printf("Enter a character\n");
+    //getting the char from user using getchar function
+    c=getchar();
+    printf("The character is\n");
+    //using the putchar to display the character
+    putchar(c);
+    
+    return 0;
+}
