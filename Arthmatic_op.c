2f5630c39d8ce3a3114df94f3f8934a0b108d2bc
sarthaksemwal <109014470+sarthaksemwal@users.noreply.github.com>
2023-08-22 21:04:55 +0530

    Create Arthmatic_op.c

---
+++ Arthmatic_op.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 5;
+
+    // Addition
+    int sum = a + b;
+    printf("Sum: %d\n", sum);
+
+    // Subtraction
+    int difference = a - b;
+    printf("Difference: %d\n", difference);
+
+    // Multiplication
+    int product = a * b;
+    printf("Product: %d\n", product);
+
+    // Division
+    int quotient = a / b;
+    printf("Quotient: %d\n", quotient);
+
+    // Modulus
+    int remainder = a % b;
+    printf("Remainder: %d\n", remainder);
+
+    return 0;
+}
