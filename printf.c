0629a73ac777801efc843c20572ab15bf052418c
sarthaksemwal <109014470+sarthaksemwal@users.noreply.github.com>
2023-08-22 20:13:53 +0530

    Update printf.c

--- printf.c
+++ printf.c
@@ -4,7 +4,9 @@ int main() {
     const char *message = "Hello, world!";
     
     // Printf is a function that we use to print the output in the screen
-    printf("message");
+    printf("Hello"\n);
+    //here \n is  a escape sequence that is used to add a new line
+    printf("World");
     
     return 0;
 }
