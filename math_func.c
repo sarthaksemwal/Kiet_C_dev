ce67c7efed278fb063ec3ab0bfed3e1e726e2ce6
sarthaksemwal <109014470+sarthaksemwal@users.noreply.github.com>
2023-08-22 21:10:46 +0530

    Create math_func.c

---
+++ math_func.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+//program starts execution form here
+
+int main() {
+    float r;
+    //using the function 
+    r=sqrt(2.0);
+    printf("the squar root of 2 is %d\n",r);
+    
+    float s;
+    //using the function 
+    
+    s=pow(2.0,8.0);
+    printf("2.0 to the power 8.0 is %d",s);
+    return 0;
+}
