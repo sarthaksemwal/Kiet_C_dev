cb9ee9bdae9f9b35692551818a9c6266f2b7c99d
sarthaksemwal <109014470+sarthaksemwal@users.noreply.github.com>
2023-08-22 20:53:41 +0530

    Create Scanf.c

---
+++ Scanf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//program starts execution form here
+
+int main() {
+    //init a int variable 
+    int a;
+    printf("Enter a integer value:\n");
+    //taking the input form the user
+    scanf("%d",&a);
+    printf("The value is :%d\n",a);
+    
+    
+    return 0;
+}
